d9cb86763574e19f42d2db9f2bfc478b4bf7e606
joenix <joenix@qq.com>
2020-07-09 15:24:47 +0800

    feat: git page

---
+++ docs/precache-manifest.39b302bcf6367ec004b410c0f0c57a67.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f2986e8ee0f0af85e71",
+    "url": "/vue-scaff-admin/css/app.2aa79a72.css"
+  },
+  {
+    "revision": "decb866f61b6e9238933",
+    "url": "/vue-scaff-admin/css/chunk-05116282.258474c6.css"
+  },
+  {
+    "revision": "3355e4e008e751441414",
+    "url": "/vue-scaff-admin/css/chunk-0b26f1ae.1d4aa08c.css"
+  },
+  {
+    "revision": "8466a24d0f00da586fb7",
+    "url": "/vue-scaff-admin/css/chunk-1ff27191.d8b73280.css"
+  },
+  {
+    "revision": "7fa62aeb6942732a52db",
+    "url": "/vue-scaff-admin/css/chunk-22c9b53a.332dd550.css"
+  },
+  {
+    "revision": "76b5f92ae79c718d4f93",
+    "url": "/vue-scaff-admin/css/chunk-290188fe.3433f1fc.css"
+  },
+  {
+    "revision": "014911305cdf1fff289c",
+    "url": "/vue-scaff-admin/css/chunk-642968b5.0b855240.css"
+  },
+  {
+    "revision": "a14b749cdaf1ff4d20f9",
+    "url": "/vue-scaff-admin/css/chunk-6eb4a59c.945c2556.css"
+  },
+  {
+    "revision": "290febd9c2aea1a19dde",
+    "url": "/vue-scaff-admin/css/chunk-vendors.78f50e9f.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/vue-scaff-admin/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/vue-scaff-admin/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "089007e721e1f22809c0313b670a36f1",
+    "url": "/vue-scaff-admin/img/401.089007e7.gif"
+  },
+  {
+    "revision": "a57b6f31fa77c50f14d756711dea4158",
+    "url": "/vue-scaff-admin/img/404.a57b6f31.png"
+  },
+  {
+    "revision": "0f4bc32b0f52f7cfb7d19305a6517724",
+    "url": "/vue-scaff-admin/img/404_cloud.0f4bc32b.png"
+  },
+  {
+    "revision": "ecba184421c1c757d6e124f4e14e29b2",
+    "url": "/vue-scaff-admin/img/avatar.ecba1844.gif"
+  },
+  {
+    "revision": "cbdbf85f1d431c45e5b70be94411fa29",
+    "url": "/vue-scaff-admin/img/banner_1.cbdbf85f.jpg"
+  },
+  {
+    "revision": "8fd21375bb16f4b48f79aadfee2f4708",
+    "url": "/vue-scaff-admin/img/banner_2.8fd21375.jpg"
+  },
+  {
+    "revision": "fdfbde50be81774b9d74fed1bdaf3b82",
+    "url": "/vue-scaff-admin/img/banner_3.fdfbde50.jpg"
+  },
+  {
+    "revision": "5c3311340031838f61ff7f04dd0cacd7",
+    "url": "/vue-scaff-admin/img/banner_4.5c331134.jpg"
+  },
+  {
+    "revision": "a7207270f8c2a6abb43ab1d43ad0c5a3",
+    "url": "/vue-scaff-admin/img/city.a7207270.png"
+  },
+  {
+    "revision": "6cf0f25b334161bda56f9262c2b5773a",
+    "url": "/vue-scaff-admin/img/empty.6cf0f25b.gif"
+  },
+  {
+    "revision": "83d79eed1503f37d51f67e8a9b9262ce",
+    "url": "/vue-scaff-admin/img/ewizard.83d79eed.gif"
+  },
+  {
+    "revision": "9ff5f675e5a7aef8971cb812c21b5bdf",
+    "url": "/vue-scaff-admin/img/logo.9ff5f675.png"
+  },
+  {
+    "revision": "9418be6fa59ec8fa408bc4059712b984",
+    "url": "/vue-scaff-admin/index.html"
+  },
+  {
+    "revision": "6f2986e8ee0f0af85e71",
+    "url": "/vue-scaff-admin/js/app.2a5bc2fd.js"
+  },
+  {
+    "revision": "decb866f61b6e9238933",
+    "url": "/vue-scaff-admin/js/chunk-05116282.f21e77e7.js"
+  },
+  {
+    "revision": "3355e4e008e751441414",
+    "url": "/vue-scaff-admin/js/chunk-0b26f1ae.79cadc32.js"
+  },
+  {
+    "revision": "8466a24d0f00da586fb7",
+    "url": "/vue-scaff-admin/js/chunk-1ff27191.ea32faee.js"
+  },
+  {
+    "revision": "7fa62aeb6942732a52db",
+    "url": "/vue-scaff-admin/js/chunk-22c9b53a.edee5ea0.js"
+  },
+  {
+    "revision": "76b5f92ae79c718d4f93",
+    "url": "/vue-scaff-admin/js/chunk-290188fe.0801a2d4.js"
+  },
+  {
+    "revision": "894660fb319ff136765e",
+    "url": "/vue-scaff-admin/js/chunk-2d0d6ad7.f4bdc7b0.js"
+  },
+  {
+    "revision": "3e82d60abb60ed5d7354",
+    "url": "/vue-scaff-admin/js/chunk-2d22a0ea.d722b7ab.js"
+  },
+  {
+    "revision": "014911305cdf1fff289c",
+    "url": "/vue-scaff-admin/js/chunk-642968b5.128706a7.js"
+  },
+  {
+    "revision": "a14b749cdaf1ff4d20f9",
+    "url": "/vue-scaff-admin/js/chunk-6eb4a59c.9c80d7b0.js"
+  },
+  {
+    "revision": "290febd9c2aea1a19dde",
+    "url": "/vue-scaff-admin/js/chunk-vendors.f5525536.js"
+  },
+  {
+    "revision": "b64bb2f4c442f663d8a6a748a201683c",
+    "url": "/vue-scaff-admin/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-scaff-admin/robots.txt"
+  }
+]);
